3a54007955759f1abf5b1b0ae9ce76a07c5d35d8
Jamie Zhu <Zqijing0815@gmail.com>
2020-05-12 16:26:34 +0000

    Initial commit

---
+++ desserts.py
@@ -0,0 +1,66 @@
+"""Dessert classes."""
+
+
+class Cupcake:
+    """A cupcake."""
+
+    # a dictionary to store all cupcake instances by name
+    cache = {}
+
+    def __repr__(self):
+        """Human-readable printout for debugging."""
+
+        return f'<Cupcake name="{self.name}" qty={self.qty}>'
+
+    def __init__(self, name, flavor, price):
+
+        self.name = name
+        self.flavor = flavor
+        self.price = price
+        self.qty = 0
+
+        self.cache[name] = self
+
+    def add_stock(self, amount):
+
+        self.qty += amount
+
+    def sell(self, amount):
+
+        if self.qty == 0:
+            print('Sorry, these cupcakes are sold out')
+            return
+
+        if self.qty < amount:
+            self.qty = 0
+            return
+
+        self.qty -= amount
+
+    @staticmethod
+    def scale_recipe(ingredients, amount):
+
+        return [(ingredients, qty * amount) 
+                for ingredients, qty in ingredients]
+
+    @classmethod
+    def get(cls, name):
+
+        if name not in cls.cache:
+            print('Sorry, that cupcake doesn\'t exist')
+            return
+
+        return cls.cache[name]
+
+
+if __name__ == '__main__':
+    import doctest
+
+    result = doctest.testfile('doctests.py',
+                              report=False,
+                              optionflags=(
+                                  doctest.REPORT_ONLY_FIRST_FAILURE
+                              ))
+    doctest.master.summarize(1)
+    if result.failed == 0:
+        print('ALL TESTS PASSED')
